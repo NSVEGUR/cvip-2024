5c2700c42d8122473622456c635183138893b63d
NSVEGUR <cs20b1016@iiitdm.ac.in>
2024-06-03 18:48:32 +0530

    feat: added hotels

--- src/app/venue-and-travel/accommodation/Hotels-With-Images.tsx
+++ src/app/venue-and-travel/accommodation/Hotels-With-Images.tsx
@@ -12,35 +12,35 @@ export function HotelsWithImages() {
   const data = [
     {
       img: TripAdvisor,
-      link: "",
+      link: "https://www.tripadvisor.in/Hotels-g4093423-Kelambakkam_Kanchipuram_District_Tamil_Nadu-Hotels.html",
     },
     {
       img: Goibibo,
-      link: "",
+      link: "https://www.goibibo.com/hotels/hotels-in-chennai/kelambakkam-ah/",
     },
     {
       img: Trivago,
-      link: "",
+      link: "https://www.trivago.in/en-IN/oar/hotel-oyo-rooms-kelambakkam-chennai?search=100-7323266",
     },
     {
       img: ClearTrip,
-      link: "",
+      link: "https://www.cleartrip.com/hotels/india/chennai/landmark/tambaram-mofussil-bus-stand?/",
     },
     {
       img: Yatra,
-      link: "",
+      link: "https://www.yatra.com/hotels/hotels-at-kelambakkam-in-chennai",
     },
     {
       img: EasyMyTrip,
-      link: "",
+      link: "https://www.easemytrip.com/hotels/hotels-in-vandalur/?e=202463184736&city=Vandalur,%20Chennai,%20Tamil%20Nadu,%20India&cin=03/07/2024&cOut=04/07/2024&Hotel=NA&Rooms=1&pax=2",
     },
     {
       img: MakeMyTrip,
-      link: "",
+      link: "https://www.makemytrip.com/hotels/hotels-in-vandalur-chennai.html",
     },
     {
       img: Expedia,
-      link: "",
+      link: "https://www.expedia.co.in/Chennai-Vandalur-Station-Hotels.d6251041.Travel-Guide-Hotels",
     },
   ];
   return (
